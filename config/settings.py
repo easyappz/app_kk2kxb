3dbb7460c179abfba087d688940f8af7ca553e21
IbragimKhaniev <newinterny@gmail.com>
2025-11-24 03:03:11 +0300

    fix: change REST_FRAMEWORK authentication classes from tuple to list

--- config/settings.py
+++ config/settings.py
@@ -73,9 +73,9 @@ INSTALLED_APPS = [
 # REST Framework configuration
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_AUTHENTICATION_CLASSES": (
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
 }
